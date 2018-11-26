a6bb5aa92bcd2bd44d2713a26e12bc6f7bb7b22b
Trang Hải <tranghai.khtn@outlook.com>
2018-11-26 23:22:59 +0700

    Adds config swagger json

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const logger = require('morgan'); 
+const logger = require('morgan');
 const router = require('./config/routes');
 const config = require('./config/dev');
 const bodyParser = require('body-parser');
+
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./config/swagger.json');
 
 mongoose.connect(config.DB_URI);
 
@@ -15,17 +18,25 @@ const PORT = 3000;
 app.use(bodyParser.json());
 app.use(express.urlencoded());
 app.use(logger('dev'));
+app.use(
+    '/api-docs',
+    swaggerUi.serve,
+    swaggerUi.setup(swaggerDocument, {
+        explorer: true,
+    })
+);
+
 app.use('/api', router);
-app.use((req, res, next) =>{
+app.use((req, res, next) => {
     const error = new Error('Not Found');
     error.status = 400;
     error.message = 'Invalid route';
     next(error);
 });
 
-app.use((req, res, next) =>{
+app.use((req, res, next) => {
     res.status(error.status || 500);
-    return res.json({ error : { message : error.message }}); 
+    return res.json({ error: { message: error.message } });
 });
 
 //Create custom middleware in Express
